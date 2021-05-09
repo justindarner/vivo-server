068e51528fa633858c32f15a7e70c31d2bf19482
Justin Darner <justin.darner@gmail.com>
2021-05-09 16:09:54 -0500

    Added basic typescript express server

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+// @ts-ignore
+app.options("*", cors());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send({ ok: true });
+});
+
+// @ts-ignore
+app.use((err, req, res, next) => {
+  const { status, ...body } = err;
+  res.status(status || 500).send(body);
+});
+
+app.listen(3000);
